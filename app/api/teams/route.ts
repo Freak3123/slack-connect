b7111bebcc7328cf7685abdf0b8795571677aa00
freak3123 <snehashradha1234@gmaul.com>
2025-08-11 03:33:04 +0530

    fix 2

--- app/api/teams/route.ts
+++ app/api/teams/route.ts
@@ -3,10 +3,11 @@ import { NextResponse } from "next/server";
 import axios from "axios";
 
 
-export async function GET() {
+export async function GET(req:Request) {
   try {
+    console.log("jhdsfghsgsgdjfghsuy--g-")
     console.log(process.env.BACKEND_URL);
-    const backendRes = await axios.get(`${process.env.BACKEND_URL}/teams`, {
+    const backendRes = await axios.get(`http://localhost:3001/teams`, {
       headers: { "Content-Type": "application/json" },
     });
 
